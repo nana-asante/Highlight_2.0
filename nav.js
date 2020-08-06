1b52722b1ef5fb0614058572832fcf73efe4d319
nana-asante <68672002+nana-asante@users.noreply.github.com>
2020-08-06 14:04:02 -0500

    Initial commit

---
+++ nav.js
@@ -0,0 +1,27 @@
+//console.clear();
+
+const navSlide= ()=> {
+    const burger   = document.querySelector('.burger');
+    const nav      = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //Toggle Nav    
+    burger.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+    
+        navLinks.forEach((link,index) =>{
+            if(link.style.animation) {
+                link.style.animation="";
+            } else {
+                link.style.animation=`navLinkFade 0.1s ease forwards 0.2s`;
+            }
+
+            //console.log(index / 7);
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
